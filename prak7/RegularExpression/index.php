4d02305393380d8eabbeee179ebbfcbacc0e3d37
toby <tobymanurung01@gmail.com>
2023-11-18 07:51:54 +0700

    commit on regexexpression indexphp

---
+++ prak7/RegularExpression/index.php
@@ -0,0 +1,19 @@
+<!-- Praktikum Pemrograman Web Minggu 7 -->
+<!-- saya mencoba praktikum bagian regular expression dengan beberapa modifikasi yaitu menggunakan lagu/musik sebagai kasus studi  -->
+<!-- 
+Nama : Toby Manurung 
+NIM : (121140099) 
+Kelas : RB / RB (Siakad/Praktikum)
+<?php
+// Teks html 
+$htmlText = '<p> Hello there, what is your favorite music genre out of these 3? <br/> <span style="text-decoration: underline;;"> Hip Hop </span> atau <span style="text-decoration: underline;"> Pop </span> ... </p>';
+
+// regex ini berguna untuk mencocokkan tag <span> dengan properti warna
+$regex = '/<span style="text-decoration:\s*([^"]+)">/';
+
+$newTxtDec = 'overline';
+
+$newHTMLText = preg_replace($regex, 'span style="text-decoration : '. $newTxtDec . ';">', $htmlText);
+
+echo $newHTMLText;
+?>
